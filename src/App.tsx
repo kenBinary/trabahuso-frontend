60609789632ca842878c5b32811c7ed2a0348295
thennek0 <kennethbecaro0@gmail.com>
2024-04-08 18:19:16 +0800

    feat: add insight page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import InsightPage from "./pages/InsightPage";
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Routes>
-        <Route path="/" element={<div>home</div>}></Route>
+        <Route path="/" element={<InsightPage></InsightPage>}></Route>
         <Route path="/methodology" element={<div>methodology</div>}></Route>
       </Routes>
     </>
