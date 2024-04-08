9861ba4b1a354f34e14dc214a3b301f141e68f6a
thennek0 <kennethbecaro0@gmail.com>
2024-04-08 18:20:33 +0800

    feat: insight page

---
+++ src/pages/InsightPage.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+import Introduction from "../components/insight-page/Introduction";
+export default function InsightPage() {
+  return (
+    <Box as="main">
+      <Introduction></Introduction>
+    </Box>
+  );
+}
