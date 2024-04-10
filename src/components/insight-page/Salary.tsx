02f6b70c10f4158ff106126e80a58e9e4c7f0d49
thennek0 <kennethbecaro0@gmail.com>
2024-04-10 21:16:11 +0800

    feat: salary data fetch

--- src/components/insight-page/Salary.tsx
+++ src/components/insight-page/Salary.tsx
@@ -1,13 +1,50 @@
-import { Box, Heading, VStack, Text, Badge } from "@chakra-ui/react";
-import { SimpleBarChart, testData } from "../charts";
+import {
+  Box,
+  Heading,
+  VStack,
+  Text,
+  Badge,
+  Spinner,
+  Flex,
+} from "@chakra-ui/react";
+import { SimpleBarChart } from "../charts";
+import useFetchOnMount from "../../hooks/useFetchOnMount";
+
+interface SalaryRange {
+  range: string;
+  count: number;
+}
+interface SalaryData {
+  undisclosed: number;
+  disclosed: Array<SalaryRange>;
+}
 
 export default function Salary() {
+  const url = "http://localhost:3000/api/job-salaries";
+  const { isLoading, isError, data } = useFetchOnMount<SalaryData>(url, {
+    undisclosed: 0,
+    disclosed: [],
+  });
+
   return (
     <VStack align="start" px="56" py="4">
       <Heading>Salary</Heading>
-      <Box w="full" height="xl">
-        <SimpleBarChart data={testData}></SimpleBarChart>
-      </Box>
+
+      <Flex w="full" height="xl" justifyContent="center" alignItems="center">
+        {isLoading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : isError ? (
+          <Heading>Failed to retrieve data</Heading>
+        ) : (
+          <SimpleBarChart data={data.disclosed}></SimpleBarChart>
+        )}
+      </Flex>
 
       <Box>
         <Badge colorScheme="green" variant="solid" letterSpacing="md">
