9e39b337c34b1eb1de31ba429a5e12a27efc2bf1
thennek0 <kennethbecaro0@gmail.com>
2024-04-10 14:05:54 +0800

    feat: add fetch on mount hook

---
+++ src/hooks/useFetchOnMount.ts
@@ -0,0 +1,70 @@
+import { useEffect, useReducer } from "react";
+type Action<T> =
+  | { type: "FETCH_INIT" }
+  | { type: "FETCH_FAIL" }
+  | { type: "FETCH_SUCCESS"; data: T };
+
+interface FetchState<T> {
+  isLoading: boolean;
+  isError: boolean;
+  data: T;
+}
+
+function fetchDataReducer<T>(
+  state: FetchState<T>,
+  action: Action<T>
+): FetchState<T> {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_FAIL":
+      return { ...state, isLoading: false, isError: true };
+    case "FETCH_SUCCESS":
+      return { ...state, isLoading: false, isError: false, data: action.data };
+    default:
+      return { ...state };
+  }
+}
+
+export default function useFetchOnMount<T>(
+  url: string,
+  initialData: T
+): FetchState<T> {
+  const [state, dispatch] = useReducer(fetchDataReducer<T>, {
+    isLoading: true,
+    isError: false,
+    data: initialData,
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+
+    async function fetchData() {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+        });
+
+        if (!response.ok) {
+          dispatch({ type: "FETCH_FAIL" });
+        }
+
+        const data = await response.json();
+        if (!didCancel && response.ok) {
+          dispatch({ type: "FETCH_SUCCESS", data: data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAIL" });
+        }
+      }
+    }
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return state;
+}
