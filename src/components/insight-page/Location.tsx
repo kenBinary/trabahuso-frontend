0123e5eabc7d76d69113ff9267e279a62ea35688
thennek0 <kennethbecaro0@gmail.com>
2024-04-11 21:13:45 +0800

    feat: add location data to charts

--- src/components/insight-page/Location.tsx
+++ src/components/insight-page/Location.tsx
@@ -1,18 +1,66 @@
-import { Heading, VStack, Box } from "@chakra-ui/react";
-import { SimpleBarChart, testData } from "../charts";
+import { Heading, VStack, Spinner, Flex } from "@chakra-ui/react";
+import { SimpleBarChart } from "../charts";
+import useFetchOnMount from "../../hooks/useFetchOnMount";
+
+interface JobLocation {
+  location: string;
+  jobCount: number;
+  medianSalary: number;
+}
 
 export default function Location() {
+  const url = "http://localhost:3000/api/job-locations/";
+  const { isLoading, isError, data } = useFetchOnMount<Array<JobLocation>>(
+    url,
+    []
+  );
+
   return (
     <VStack px="56" align="start" py="4" gap="2">
       <Heading>Location</Heading>
 
       <Heading as="h4" size="md">
-        Job frequency by province
+        Job frequency by location
       </Heading>
 
-      <Box w="full" height="xl">
-        <SimpleBarChart data={testData}></SimpleBarChart>
-      </Box>
+      <Flex w="full" height="xl" justifyContent="center" alignItems="center">
+        {isLoading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : isError ? (
+          <Heading>Failed to retrieve data</Heading>
+        ) : (
+          <SimpleBarChart data={data} barDataKey="jobCount"></SimpleBarChart>
+        )}
+      </Flex>
+
+      <Heading as="h4" size="md">
+        Median Salary by location
+      </Heading>
+
+      <Flex w="full" height="xl" justifyContent="center" alignItems="center">
+        {isLoading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : isError ? (
+          <Heading>Failed to retrieve data</Heading>
+        ) : (
+          <SimpleBarChart
+            data={data}
+            barDataKey="medianSalary"
+          ></SimpleBarChart>
+        )}
+      </Flex>
     </VStack>
   );
 }
