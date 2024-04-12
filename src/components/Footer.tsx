f1f1403569dbef59b5db388cbe71c36c67ee9be8
thennek0 <kennethbecaro0@gmail.com>
2024-04-12 22:48:12 +0800

    feat: add footer style and link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
-import { Flex, Icon } from "@chakra-ui/react";
+import { Flex, Icon, Link } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <Flex as="footer" align="center" flexDirection="column" px="56" py="3">
-      <Icon boxSize="8" as={FaGithub} />
+    <Flex
+      as="footer"
+      align="center"
+      flexDirection="column"
+      px="56"
+      py="3"
+      bgColor="brand.accent"
+    >
+      <Link href="https://github.com/kenBinary" target="_blank">
+        <Icon boxSize="8" as={FaGithub} />
+      </Link>
     </Flex>
   );
 }
