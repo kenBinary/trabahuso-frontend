e988238bb226b5b0a8761a86c66308430cd0f601
thennek0 <kennethbecaro0@gmail.com>
2024-04-28 15:54:33 +0800

    refactor: change endpoints to only base url

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,20 +1,9 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_HELLO: string;
   readonly VITE_JOBS_ENDPOINT: string;
-  readonly VITE_JOB_FREQUENCY_LIMIT_ENDPOINT: string;
-  readonly VITE_JOB_FREQUENCY_ENDPOINT: string;
+  readonly VITE_JOB_LOCATIONS_ENDPOINT: string;
   readonly VITE_SALARY_RANGE_ENDPOINT: string;
-  readonly VITE_PROG_LANG_ENDPOINT: string;
-  readonly VITE_PROG_LANG_LIMIT_ENDPOINT: string;
-  readonly VITE_DB_ENDPOINT: string;
-  readonly VITE_DB_LIMIT_ENDPOINT: string;
-  readonly VITE_FRAME_AND_LIBS_ENDPOINT: string;
-  readonly VITE_FRAME_AND_LIBS_LIMIT_ENDPOINT: string;
-  readonly VITE_CLOUD_PLAT_ENDPOINT: string;
-  readonly VITE_CLOUD_PLAT_LIMIT_ENDPOINT: string;
-  readonly VITE_TOOLS_ENDPOINT: string;
-  readonly VITE_TOOLS_LIMIT_ENDPOINT: string;
+  readonly VITE_TECH_STACK_ENDPOINT: string;
   // more env variables...
 }
 
